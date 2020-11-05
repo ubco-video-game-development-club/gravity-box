2e77ff3ef74bc06f98b6eb373931652ee13125b4
Jaden Balogh <jadenbalogh@gmail.com>
2020-11-05 10:59:41 -0800

    Add formatting example code

--- Assets/Scripts/FormatExample.cs
+++ Assets/Scripts/FormatExample.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Example: rigidbody-based jumping script
 public class FormatExample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Example: inspector field not settable by other scripts
+    [SerializeField] private int jumpSpeed;
+    public int JumpSpeed { get { return jumpSpeed; } }
+
+    // Example: private variable not gettable by other scripts
+    private bool isGrounded;
+
+    // Example: cached Component
+    private Rigidbody2D rb2D;
+    // Note: use transform directly
+
+    void Awake()
     {
-        
+        // Variable initialization and component caching
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Start-up functionality and object referencing
+    }
+
     void Update()
     {
-        
+        // LINQ: good for select/filter/etc but use loops for iterating
     }
 }
