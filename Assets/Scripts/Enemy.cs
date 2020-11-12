d0f665153c8b59005b5f5190f2aba16f8667a67d
Kurtis Knodel <kurtisknodel@gmail.com>
2020-11-12 11:28:47 -0800

    Created enemy script
    This handles enemy health and death.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health { get { return currentHealth; } }
+
+    [SerializeField] private float maxHealth = 10.0f;
+    [SerializeField] private UnityEvent onDie;
+
+    private float currentHealth;
+
+    void Update() 
+    {
+        if (currentHealth <= 0.0f)
+        {
+            onDie.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddOnDieListener(UnityAction listener)
+    {
+        onDie.AddListener(listener);
+    }
+
+    public void RemoveOnDieListener(UnityAction listener)
+    {
+        onDie.RemoveListener(listener);
+    }
+
+}
