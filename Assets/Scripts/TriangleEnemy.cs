d802cfa87a6efe1436202d3adfbb9d2a1b3eb0d4
Kurtis Knodel <kurtisknodel@gmail.com>
2020-11-12 11:38:02 -0800

    Enemies now move towards the player

---
+++ Assets/Scripts/TriangleEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TriangleEnemy : Enemy
+{
+    [SerializeField] private float launchForce = 5.0f;
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player == null)
+        {
+            Debug.LogWarning("Object with tag \"Player\" not found.");
+            return;
+        }
+
+        Vector2 toPlayer = (player.transform.position - this.transform.position).normalized;
+        GetComponent<Rigidbody2D>().AddForce(toPlayer * launchForce, ForceMode2D.Impulse);
+    }
+}
