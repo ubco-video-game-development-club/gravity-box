ed119757cc71f9c6801ccbd1019dd2a67a2e7b5c
Kurtis Knodel <kurtisknodel@gmail.com>
2020-11-12 12:48:48 -0800

    Created MainMenuManager

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Text highscoreText;
+
+    void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("player.highscore");
+        highscoreText.text = $"High Score: {highScore}";
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        //TODO: Load the scene in a more elegant way (?)
+        SceneManager.LoadScene("Game");
+    }
+}
