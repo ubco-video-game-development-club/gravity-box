5a8675ae5c1da156fa05e44be18cb833be363633
Jaden Balogh <jadenbalogh@gmail.com>
2020-11-19 13:00:06 -0800

    Update damage formula and add direct hits

--- Assets/Scripts/Game/Entity/Rocket.cs
+++ Assets/Scripts/Game/Entity/Rocket.cs
@@ -8,7 +8,8 @@ public class Rocket : MonoBehaviour
     [SerializeField] private float speed = 1;
     [SerializeField] private float explosionStrength = 1;
     [SerializeField] private float explosionRadius = 1;
-    [SerializeField] private float baseDamage = 10.0f;
+    [SerializeField] private float minDamage = 3.0f;
+    [SerializeField] private float maxDamage = 10.0f;
 
     public Vector2 Direction
     {
@@ -36,26 +37,40 @@ public class Rocket : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // Get enemy hit if we did direct hit
+        Enemy hitEnemy;
+        bool didDirectHit = col.TryGetComponent<Enemy>(out hitEnemy);
+
         // Apply explosion to all targets within range
         Vector3 explosionPos = transform.position;
         Collider2D[] targets = Physics2D.OverlapCircleAll(explosionPos, explosionRadius);
         foreach (Collider2D target in targets)
         {
-            Rigidbody2D targetBody;
-            if (target.TryGetComponent<Rigidbody2D>(out targetBody))
+            if (target.TryGetComponent<Rigidbody2D>(out Rigidbody2D targetBody))
             {
                 Vector2 forceOffset = targetBody.transform.position - explosionPos;
                 Vector2 forceDir = forceOffset.normalized;
                 float forceDist = forceOffset.magnitude;
                 float forceScale = 1 - Mathf.Clamp(forceDist / explosionRadius, 0f, 1f);
-                targetBody.AddForce(forceScale * forceDir * explosionStrength, ForceMode2D.Impulse);
+                targetBody.AddForce(explosionStrength * forceDir * forceScale, ForceMode2D.Impulse);
             }
 
-            if(target.TryGetComponent<Enemy>(out Enemy enemy))
+            if (target.TryGetComponent<Enemy>(out Enemy enemy))
             {
-                float distance = Vector2.Distance(enemy.transform.position, this.transform.position);
-                float damage = baseDamage / (1.0f + distance);
-                enemy.TakeDamage(damage);
+                // Check if we direct hit this enemy
+                if (didDirectHit && enemy == hitEnemy)
+                {
+                    // Apply full damage
+                    enemy.TakeDamage(maxDamage);
+                }
+                else
+                {
+                    // Damage based on distance
+                    float enemyDist = Vector2.Distance(enemy.transform.position, transform.position);
+                    float damageScale = 1 - Mathf.Clamp(enemyDist / explosionRadius, 0f, 1f);
+                    float damage = Mathf.Lerp(minDamage, maxDamage, damageScale);
+                    enemy.TakeDamage(damage);
+                }
             }
         }
 
